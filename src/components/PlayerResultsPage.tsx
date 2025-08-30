71c46744430f4ca4aafe8ebbeb2a88e64196f1f6
“Sam <samdeering@gmail.com>
2025-08-30 20:07:57 +1000

    Remove shadow from main content container and add white background to outer container

--- src/components/PlayerResultsPage.tsx
+++ src/components/PlayerResultsPage.tsx
@@ -121,9 +121,9 @@ const PlayerResultsPage: React.FC<PlayerResultsPageProps> = ({ propPlayerId }) =
   }
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-white dark:bg-[#121213]">
       {/* Main Content Area - Responsive Layout */}
-      <div className="px-4 lg:px-0 bg-white dark:bg-[#121213] rounded-lg shadow-sm">
+      <div className="px-4 lg:px-0 bg-white dark:bg-[#121213] rounded-lg">
         {player ? (
           <>
             {/* Row 1 - Main player information */}
